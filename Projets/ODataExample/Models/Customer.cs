c240336067fbb173781bd1a7027d9c3e468eb003
Forandos <math.comandini@outlook.fr>
2023-12-26 20:36:12 +0100

    example odata

---
+++ Projets/ODataExample/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace ODataExample.Models
+{
+    using System.Collections.Generic;
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
