0cb04c70daf8352c215137283c5a24f2407923ae
Forandos <math.comandini@outlook.fr>
2024-01-06 23:52:05 +0100

    mutation set up

---
+++ Projets/5 - Mutation/Models/EmployeeQuery.cs
@@ -0,0 +1,62 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System.Security.Cryptography;
+
+namespace _5___Mutation.Models;
+
+public class EmployeeQuery : ObjectGraphType
+{
+    [Obsolete]
+    public EmployeeQuery(IEmployeeService employeeService)
+    {
+        Field<ListGraphType<EmployeeDetailsType>>(Name = "Employees", resolve: x => employeeService.GetEmployees());
+        Field<ListGraphType<EmployeeDetailsType>>(Name = "Employee",
+            arguments: new QueryArguments(new QueryArgument<IntGraphType> { Name = "id" }),
+            resolve: x => employeeService.GetEmployee(x.GetArgument<int>("id")));
+    }
+}
+
+public class EmployeeMutation : ObjectGraphType
+{
+    [Obsolete]
+    public EmployeeMutation(EmployeeData data)
+    {
+        Field<EmployeeInputType>("createEmployee")
+            .Argument<NonNullGraphType<EmployeeInputType>>("employee")
+            .Resolve(context => 
+            {
+                var employee = context.GetArgument<Employee>("employee");
+                return data.AddEmployee(employee);
+            });
+
+    }
+}
+
+public class EmployeeDetailsSchema : Schema
+{
+    public EmployeeDetailsSchema(IServiceProvider serviceProvider) : base(serviceProvider)
+    {
+        Query = serviceProvider.GetRequiredService<EmployeeQuery>();
+        Mutation = serviceProvider.GetRequiredService<EmployeeMutation>();
+    }
+}
+public class EmployeeData
+{
+    private List<Employee> _employees = new List<Employee>();
+
+    public Employee AddEmployee(Employee employee)
+    {
+        _employees.Add(employee);
+        return employee;
+    }
+}
+
+public class EmployeeInputType : InputObjectGraphType
+{
+    public EmployeeInputType()
+    {
+        Name = "EmployeeInput";
+        Field<NonNullGraphType<StringGraphType>>("name");
+        Field<StringGraphType>("age");
+    }
+}
--- Projets/5 - Mutation/Program.cs
+++ Projets/5 - Mutation/Program.cs
@@ -1,3 +1,7 @@
+using _5___Mutation.Models;
+using GraphQL.Types;
+using GraphQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IEmployeeService, EmployeeService>();
+builder.Services.AddSingleton<EmployeeDetailsType>();
+builder.Services.AddSingleton<EmployeeInputType>();
+builder.Services.AddSingleton<EmployeeData>();
+builder.Services.AddSingleton<EmployeeQuery>();
+builder.Services.AddSingleton<EmployeeMutation>();
+builder.Services.AddSingleton<ISchema, EmployeeDetailsSchema>();
+builder.Services.AddGraphQL(b => b
+    .AddAutoSchema<EmployeeQuery>() // schema
+    .AddSystemTextJson());   // serializer-
 
 var app = builder.Build();
 
@@ -19,5 +34,14 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseGraphQL<ISchema>("/graphql");            // url to host GraphQL endpoint
+app.UseGraphQLPlayground(
+    "/",                               // url to host Playground at
+    new GraphQL.Server.Ui.Playground.PlaygroundOptions
+    {
+        GraphQLEndPoint = "/graphql",         // url of GraphQL endpoint
+        SubscriptionsEndPoint = "/graphql",   // url of GraphQL endpoint
+    });
 
 app.Run();
