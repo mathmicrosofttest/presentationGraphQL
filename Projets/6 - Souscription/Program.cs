f9382f0477fd96ed4066e556d62f9edacbcdf2f4
Forandos <math.comandini@outlook.fr>
2024-01-07 11:45:39 +0100

    subscription set up

--- Projets/6 - Souscription/Program.cs
+++ Projets/6 - Souscription/Program.cs
@@ -1,3 +1,7 @@
+using GraphQL.Types;
+using GraphQL;
+using GraphQL.Tests.Subscription;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IChat, Chat>();
+builder.Services.AddSingleton<MessageInputType>();
+builder.Services.AddSingleton<MessageFromType>();
+builder.Services.AddSingleton<ISchema, ChatSchema>();
+builder.Services.AddGraphQL(b => b
+    .AddAutoSchema<ChatSubscriptions>() // schema
+    .AddSystemTextJson());   // serializer-
 
 var app = builder.Build();
 
